ea7899006ec32dfcf6919cb0207b2f6ec1769082
AnaelGor <anaellegorfinkel@gmail.com>
2019-04-29 14:14:59 +0300

    menu bar

--- game.js
+++ game.js
@@ -63,6 +63,18 @@ function Start() {
     interval = setInterval(UpdatePosition, 200);
 }
 
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft = "0";
+}
+
 
 function findRandomEmptyCell(board) {
     var i = Math.floor((Math.random() * 9) + 1);
