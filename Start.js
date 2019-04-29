b130e6a73bf8f69d34ae2722f326330d2e014905
AnaelGor <anaellegorfinkel@gmail.com>
2019-04-29 15:02:09 +0300

    logo

---
+++ Start.js
@@ -0,0 +1,22 @@
+
+function PageLoaded()
+{
+    ShowSection('Welcome');
+}
+function ShowDiv(id)
+{
+    //hide all sections
+    var section1 = document.getElementById('Welcome');
+    section1.style.visibility="hidden";
+    var section2 = document.getElementById('Settings');
+    section2.style.visibility="hidden";
+    var section3 = document.getElementById('My Account');
+    section3.style.visibility="hidden";
+    var section4 = document.getElementById('About');
+    section4.style.visibility="hidden";
+
+    //show only one section
+    var selected = document.getElementById(id);
+    selected .style.visibility="visible";
+}
+
