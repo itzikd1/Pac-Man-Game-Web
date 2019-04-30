4c9166fa5546b855a4cb5a8d9e7007dfd1da8611
itzik <itzikd15@gmail.com>
2019-04-30 16:16:05 +0300

    register done +-

--- register.js
+++ register.js
@@ -1,15 +1,35 @@
 var reg2 = new Array();
 
+function check_uname() {
+    var pattern = /^[a-zA-Z]*$/;
+    var uname = $("#form_uname").val();
+    if (pattern.test(uname) && uname !== '') {
+        $("#form_uname").css("border-bottom", "2px solid #34F458");
+    } else {
+        $("#form_uname").css("border-bottom", "2px solid #F90A0A");
+        error_uname = true;
+    }
+}
+
+function check_password() {
+    var pattern = /^[a-zA-Z]*$/;
+    var pattern2 = /^[0-9]*$/;
+    var password_length = $("#form_password").val().length;
+    var password = $("#form_password").val();
+    if (password_length < 8 || pattern.test(password) || pattern2.test(password)) {
+        $("#form_password").css("border-bottom", "2px solid #F90A0A");
+        error_password = true;
+    } else {
+        $("#form_password").css("border-bottom", "2px solid #34F458");
+    }
+}
+
 function check_fname() {
     var pattern = /^[a-zA-Z]*$/;
     var fname = $("#form_fname").val();
-    var fname = $("#form_fname").val();
     if (pattern.test(fname) && fname !== '') {
-        $("#fname_error_message").hide();
         $("#form_fname").css("border-bottom", "2px solid #34F458");
     } else {
-        $("#fname_error_message").html("Should contain only Characters");
-        $("#fname_error_message").show();
         $("#form_fname").css("border-bottom", "2px solid #F90A0A");
         error_fname = true;
     }
@@ -19,71 +39,51 @@ function check_sname() {
     var pattern = /^[a-zA-Z]*$/;
     var sname = $("#form_sname").val()
     if (pattern.test(sname) && sname !== '') {
-        $("#sname_error_message").hide();
         $("#form_sname").css("border-bottom", "2px solid #34F458");
     } else {
-        $("#sname_error_message").html("Should contain only Characters");
-        $("#sname_error_message").show();
         $("#form_sname").css("border-bottom", "2px solid #F90A0A");
-        error_fname = true;
+        error_sname = true;
     }
 }
 
-function check_password() {
-    var password_length = $("#form_password").val().length;
-    if (password_length < 8) {
-        $("#password_error_message").html("Atleast 8 Characters");
-        $("#password_error_message").show();
-        $("#form_password").css("border-bottom", "2px solid #F90A0A");
-        error_password = true;
-    } else {
-        $("#password_error_message").hide();
-        $("#form_password").css("border-bottom", "2px solid #34F458");
-    }
-}
-
-function check_retype_password() {
-    var password = $("#form_password").val();
-    var retype_password = $("#form_retype_password").val();
-    if (password !== retype_password) {
-        $("#retype_password_error_message").html("Passwords Did not Matched");
-        $("#retype_password_error_message").show();
-        $("#form_retype_password").css("border-bottom", "2px solid #F90A0A");
-        error_retype_password = true;
-    } else {
-        $("#retype_password_error_message").hide();
-        $("#form_retype_password").css("border-bottom", "2px solid #34F458");
-    }
-}
 
 function check_email() {
     var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     var email = $("#form_email").val();
     if (pattern.test(email) && email !== '') {
-        $("#email_error_message").hide();
         $("#form_email").css("border-bottom", "2px solid #34F458");
     } else {
-        $("#email_error_message").html("Invalid Email");
-        $("#email_error_message").show();
         $("#form_email").css("border-bottom", "2px solid #F90A0A");
         error_email = true;
     }
 }
+
+function check_date() {
+    if ($("#form_date").val() !== '') {
+        $("#form_date").css("border-bottom", "2px solid #34F458");
+    } else {
+        $("#form_date").css("border-bottom", "2px solid #F90A0A");
+        error_fdate = true;
+
+    }
+}
+
 
 function clickMe() {
     error_fname = false;
     error_sname = false;
     error_email = false;
     error_password = false;
-    error_retype_password = false;
+    error_fdate = false;
 
+    check_uname();
+    check_password();
     check_fname();
     check_sname();
     check_email();
-    check_password();
-    check_retype_password();
+    check_date();
 
-    if (error_fname === false && error_sname === false && error_email === false && error_password === false && error_retype_password === false) {
+    if (error_fname === false && error_sname === false && error_email === false && error_password === false && error_fdate === false) {
         alert("Registration Successfull");
         var person = {username:$("#form_fname").val(), password:$("#form_password").val(), lastname:$("#form_sname").val(), email:$("#form_email").val()}
         reg2.push({
